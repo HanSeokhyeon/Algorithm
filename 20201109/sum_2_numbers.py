b815d527a383ec3781092810587c9753b7e9b297
hanseokhyeon <sah0322@naver.com>
2020-11-09 13:18:44 +0900

    sum2nubers

---
+++ 20201109/sum_2_numbers.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+
+
+def solution(numbers):
+    answer = set()
+    for a, b in combinations(numbers, 2):
+        answer.add(a+b)
+    return list(sorted(answer))
